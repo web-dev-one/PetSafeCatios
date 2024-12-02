ed59bed68f1a2e599ef3b86e3d91a3612777a337
web-dev-one <solutionsllcwebmasters@gmail.com>
2024-12-01 19:20:07 -0700

    update

---
+++ javascript/pics.js
@@ -0,0 +1,40 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+const phrases = ["We Protect Your Cats", "Guaranteed Protection", "Protection You Can Purchase"]
+
+let change = document.getElementById('change');
+let i = 0;
+
+(function (){
+  window.setTimeout(changeText(), 4000);
+  i++
+})()
+
+function changeText(){}{
+    change.textContent = phrases[i]
+}
